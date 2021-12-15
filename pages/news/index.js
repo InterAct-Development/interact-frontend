da619d46317b07aae8b2cc094150266854fbe2a9
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-12-15 13:25:10 +0000

    Using NextJS Link component

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,28 @@
 // our-domain.com/news
 
+import { Fragment } from "react";
+import Link from 'next/link';
+
 function NewsPage() {
-    return <h1>The Page of News</h1>
-  }
-  
-  export default NewsPage;
+  return (
+    <Fragment>
+      <h1>The Page of News - Using React Fragment</h1>
+
+      <ul>
+        <li>
+          <Link href='/news/news_nested'>
+            News Nested
+          </Link>
+        </li>
+        
+        <li>
+          <Link href='/news/something'>
+            Dynamic Nested
+          </Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
