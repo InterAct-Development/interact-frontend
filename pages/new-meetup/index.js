27db32776e29405aff043911f49dc06688dcb896
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-12-16 18:59:01 +0000

    Initiating basic form component and temp console log form submission function

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,13 @@
 // our-domain.com/new-meetup
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+function NewMeetup() {
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetup;
