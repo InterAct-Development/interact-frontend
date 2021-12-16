fc8ecaa27ea2c4e0fd7e03761d1a5879718eca01
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-12-16 21:51:46 +0000

    Fetching Dynamic Data using getStaticProps and getStaticPaths - Temp. Hardcoded

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,4 +13,43 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/1e/Rio_De_Janeiro_-_Rafael_Defavari.jpg",
+        title: "First Meetup",
+        address: "Fake 123 Address",
+        description: "Lorem Ipsum",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
