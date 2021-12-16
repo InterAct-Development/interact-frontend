9ae034a54276115ee8369a35909373adb882f525
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-12-16 21:25:13 +0000

    Using NextJS getStaticProps() data fetching and revalidate Incremental Static Regeneration

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 // our-domain.com/
 
-import { 
-    useEffect, 
-    useState 
-} from 'react';
-
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -25,15 +20,17 @@ const DUMMY_MEETUPS = [
     }
 ];
 
-function HomePage() {
-    const [loadedMeetups, setLoadedMeetups] = useState([]);
-
-    useEffect(() => {
-        // send a http request and fetch data
-        setLoadedMeetups(DUMMY_MEETUPS);
-    }, []);
-
-    return <MeetupList meetups={loadedMeetups} />
+function HomePage(props) {
+    return <MeetupList meetups={props.meetups} />
 }
+
+export async function getStaticProps() {
+    // fetch data from an API
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        }
+    };
+};
 
 export default HomePage;
