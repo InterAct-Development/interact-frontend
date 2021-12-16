865735f308f71a37b8e5226c4e30e4b333652739
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-12-16 17:31:21 +0000

    Rendering Meetup component in main file

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,27 @@
 // our-domain.com/
+
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Rio_De_Janeiro_-_Rafael_Defavari.jpg',
+        address: 'Fake 123 St.',
+        description: 'First Meetup'
+    },
+
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Samuel_Beckett_Bridge_At_Sunset_Dublin_Ireland_%2897037639%29_%28cropped%29.jpeg',
+        address: 'Fake Ave.',
+        description: 'Second Meetup'
+    }
+];
+
+function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage;
