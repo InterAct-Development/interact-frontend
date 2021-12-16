bfaf02876c265e0d59df9bbe0b22d2b678ae69ea
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-12-16 19:41:22 +0000

    Adding temp static data to Dynamic page and using NextJS CSS Module support

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,16 @@
 // our-domain.com/[dynamic page]
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/1/1e/Rio_De_Janeiro_-_Rafael_Defavari.jpg"
+      title="First Meetup"
+      address="Fake 123 Address"
+      description="Lorem Ipsum"
+    />
+  );
+}
+
+export default MeetupDetails;
