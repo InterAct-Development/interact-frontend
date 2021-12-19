3e81411d09ee0db8aeed9983b8dfcc6d38a69dfe
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2021-12-19 22:10:51 +0000

    Connecting and Querying a MongoDB Database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,25 @@
 // api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
-    if (req.method === 'POST') {
-        const data = req.body;
+import { MongoClient } from 'mongodb';
 
-        const { title, image, address, description } = data;
-    }
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    // const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect('mongodb+srv://john:mongodbadmin@cluster0.tzfjz.mongodb.net/meetups?retryWrites=true&w=majority');
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data); // same as line 10 but in Next.js/MongoDB way
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message: 'Meetup Inserted'});
+  }
 }
 
 export default handler;
