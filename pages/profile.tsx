f8aac02b62a518502c5d63c43a891be6fdf745b1
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2022-01-18 16:15:16 +0000

    Added MUI Grids system in profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,10 +3,15 @@ import { apiRequest } from "../helpers/Requests";
 import { Body } from "../layout/Body";
 import { AppContext } from "../helpers/Context";
 import { NextPage } from "next";
-import VerticalLinearStepper from "../components/stepper/Stepper";
 import { invalidToken, Middleware } from "../helpers/Middleware";
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
 import Avatar from '../components/avatar/Avatar';
+import VerticalLinearStepper from "../components/stepper/Stepper";
 
 const Profile: NextPage = () => {
   const appContext = useContext(AppContext);
@@ -17,7 +22,7 @@ const Profile: NextPage = () => {
   useEffect(() => {
     const { auth, userId, token } = appContext.state;
     let isMounted: boolean = true;
-    
+
     // Look at a better way to do this.
     if (!auth) invalidToken();
 
@@ -48,17 +53,31 @@ const Profile: NextPage = () => {
 
   return (
     <Body>
-      <Middleware auth={appContext.state.auth}>
-        <>
-            <h1>Profile Page (Protected Route)</h1>
-            <Avatar />
-            <p>{"Name: " + name ?? ""}</p>
-            <p>{"Email: " + email ?? ""}</p>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <Middleware auth={appContext.state.auth}>
+            <>
 
-            <VerticalLinearStepper />
-        </>
-      </Middleware>
-    </Body>
+              <h1>Profile Page (Protected Route)</h1>
+
+              <Grid xs={4}>
+                <Avatar />
+              </Grid>
+
+              <Grid xs={8}>
+                <p>{"Name: " + name ?? ""}</p>
+                <p>{"Email: " + email ?? ""}</p>
+              </Grid>
+
+              <Grid xs={12}>
+                <VerticalLinearStepper />
+              </Grid>
+              
+            </>
+          </Middleware>
+        </Grid>
+      </Box>
+    </Body >
   );
 };
 
