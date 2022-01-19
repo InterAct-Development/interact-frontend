97acacce23ec6b226879ec106bdb7b05b78b701b
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2022-01-19 15:57:02 +0000

    Initiated ViewStep component for demo purposes

---
+++ components/stepper/Step.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import { deepOrange } from '@mui/material/colors';
+
+// Text
+// Images
+// Video?
+
+const ViewStep = () => {
+
+}
+
+export default ViewStep;
