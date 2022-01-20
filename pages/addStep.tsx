e47d417528fe0627877c5b6400d091f235e44c3c
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2022-01-20 16:54:24 +0000

    Added contents for static demo form

--- pages/addStep.tsx
+++ pages/addStep.tsx
@@ -1,20 +1,124 @@
 import * as React from 'react';
 import type { NextPage } from "next";
+import { useRouter } from 'next/router'
 
 import { Body } from '../layout/Body';
 
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import {
+    styled,
+    Box,
+    Paper,
+    Grid,
+    TextField,
+    Skeleton,
+    Stack,
+    IconButton,
+    Button
+} from '@mui/material'
+
+import {
+    Add,
+    AddPhotoAlternate,
+    VideoLibrary
+} from '@mui/icons-material';
 
 const AddStep: NextPage = () => {
+    const backRouter = useRouter()
+
     return (
         <Body>
             <Box sx={{ flexGrow: 1, margin: 1 }}>
                 <Grid container>
                     <>
-                        <h1>Add Step Form</h1>
+                        <Grid xs={12}>
+                            <h1>Add Step Form</h1>
+                        </Grid>
+
+                        <Grid xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                label="Step/Goal/Achievement"
+                                id="fullWidth filled-basic"
+                                variant="filled"
+                            />
+                        </Grid>
+
+                        <Grid xs={12} style={{ marginTop: 20 }}>
+                            <TextField
+                                required
+                                fullWidth
+                                multiline
+                                rows={8}
+                                label="Write something..."
+                                // defaultValue="Default Value"
+                                id="fullWidth filled-multiline-static"
+                                variant="filled"
+                            />
+                        </Grid>
+                        
+                        <Grid xs={12}>
+                            <p>
+                                Add Images
+
+                                <IconButton aria-label="add" color="primary">
+                                    <AddPhotoAlternate />
+                                </IconButton>    
+                            </p>
+                        </Grid>
+                        
+                        <Grid xs={6} style={{ marginTop: -20 }}>
+                            <Skeleton
+                                sx={{ bgcolor: 'grey.700' }}
+                                // animation="wave"
+                                variant="rectangular"
+                                // width='100%'
+                                height={240}
+                            />
+                        </Grid>
+                        
+                        <Grid xs={6} style={{ marginTop: -20 }}>
+                            <Skeleton
+                                sx={{ bgcolor: 'grey.700' }}
+                                // animation="wave"
+                                variant="rectangular"
+                                // width='100%'
+                                height={240}
+                            />
+                        </Grid>
+                        
+                        <Grid xs={12}>
+                            <p>
+                                Add Video
+
+                                <IconButton aria-label="add" color="primary">
+                                    <VideoLibrary />
+                                </IconButton>      
+                            </p>
+                        </Grid>
+                        
+                        <Grid xs={12} style={{ marginTop: -20 }}>
+                            <Skeleton
+                                sx={{ bgcolor: 'grey.700' }}
+                                // animation="wave"
+                                variant="rectangular"
+                                // width='100%'
+                                height={240}
+                            />
+                        </Grid>
+
+                        <Grid 
+                            xs={12} 
+                            style={{ display: "flex", marginTop: 20, marginBottom: 20 }}
+                        >
+                            <Button 
+                                onClick={() => backRouter.back()} 
+                                variant="contained" 
+                                style={{ marginLeft: "auto" }}
+                            >
+                                Back to Profile
+                            </Button>
+                        </Grid>
                     </>
                 </Grid>
             </Box>
